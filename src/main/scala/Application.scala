d2a68d2f05780b40ee2b6ac2caf46c3dcddaea5c
Nick <nick.tune@artirix.com>
2015-02-20 14:26:12 +0000

    added instructions to console output

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -31,11 +31,33 @@ object Application {
   }
 
   private def printInstructionsToConsole() {
-    println("PRINT instructions to console")
+    val instructions = """
+    |*************************************************************
+    |*************************************************************
+    |
+    |    Welcome to consoletweetz. The future of social media
+    |    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    |
+    |     There are six commands:
+    |       posting: <user name> -> <message>
+    |       reading: <user name>
+    |       following: <user name> follows <another user>
+    |       wall: <user name> wall
+    |       *help*: display this message
+    |       *quit*: quit the application and get back to doing some work
+    | 
+    |     Please tweet responsibly.""".stripMargin('|').trim
+    print(instructions)
+    print(">")
   }
 
   private def quitApplication() {
-    println("PRINT quit message")
+    val quitMessage = """
+    |
+    |Thankyou for using consoletweetz.
+    |Please tell all your friends and make us go viral
+    """.stripMargin('|').trim
+    println(quitMessage)
   }
 
 }
